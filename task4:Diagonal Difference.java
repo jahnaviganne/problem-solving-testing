f03fa23fd3e9dae89c83277fb8bb762370f60bf1
jahnaviganne <gannejahnavi@gmail.com>
2026-09-09 11:17:59 +0530

    Implement diagonalDifference method in Solution class

---
+++ task4:Diagonal Difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int primary = 0;
+        int secondary = 0;
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            primary += arr.get(i).get(i);
+            secondary += arr.get(i).get(n - 1 - i);
+        }
+
+        return Math.abs(primary - secondary);
+    }
+}
